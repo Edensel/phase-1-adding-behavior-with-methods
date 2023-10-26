9ec1778e089409f1885dc3f2992527ca14de5219
Esekon <densel.esekon@moringaschool.com>
2023-10-26 23:55:12 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Your code here
+class Cat {
+    constructor(name, gender) {
+      this.name = name;
+      this.gender = gender;
+    }
+  
+    speak() {
+      return `${this.name} says meow!`;
+    }
+  }
+  
+  class Dog {
+    constructor(name, gender) {
+      this.name = name;
+      this.gender = gender;
+    }
+  
+    speak() {
+      return `${this.name} says woof!`;
+    }
+  }
+  
+  class Bird {
+    constructor(name, gender) {
+      this.name = name;
+      this.gender = gender;
+    }
+  
+    speak() {
+      if (this.gender === 'male') {
+        return `It's me! ${this.name}, the parrot!`;
+      } else {
+        return `${this.name} says squawk!`;
+      }
+    }
+  }
+  
+  let cat, dog, bird, bird2;
+  
+  describe("cat", () => {
+    beforeEach(() => {
+      cat = new Cat("Sasha", "female");
+    });
+  
+    // ... rest of your test cases ...
+  });
+  
